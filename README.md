d942871bcd8a2e0601e0c7dc9acbfd7ab7a1db1d
Дмитрий Морозов <ADM713934@yandex.ru>
2023-07-09 13:18:08 +0300

    1st review

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# scrapy_parser_pep
+## Описание проекта
+Асинхронный парсер, собирающий данные (номер, название и статус) о Python Enhancement Proposals (PEP) с сайта https://www.python.org/, а также формирующий сводку по количеству PEP каждого статуса в отдельный .csv файл.
+
+### Технологии
+- Python 3.11.0
+- Scrapy 2.5.1
+
+**Для работы с парсером**:
+>Скопировать проект командой: 
+```bash
+git clone git@github.com:Esposus/scrapy_parser_pep.git
+```
+>Установить и активировать виртуальное окружение
+```bash
+python3 -m venv venv
+
+source venv/Scripts/activate
+```
+>Установить зависимости:
+```bash
+pip install -r requirements.txt
+```
+>Запустить из директории scrapy_parser_pep:
+```bash
+scrapy crawl pep
+```
+
+### Автор: [Дмитрий Морозов](https://github.com/Esposus "GitHub аккаунт")
--- pep_parse/spiders/pep.py
+++ pep_parse/spiders/pep.py
@@ -1,3 +1,5 @@
+from urllib.parse import urljoin
+
 import scrapy
 
 from pep_parse.items import PepParseItem
@@ -5,14 +7,19 @@ from pep_parse.items import PepParseItem
 
 class PepSpider(scrapy.Spider):
     name = 'pep'
-    allowed_domains = ['peps.python.org']
-    start_urls = ['https://peps.python.org/']
+    allowed_domains = ['peps.python.org'] 
+    start_urls = ['https://peps.python.org/'] 
 
     def parse(self, response):
         """Собирает ссылки на документы PEP"""
-        peps = response.css('section[id=numerical-index] tbody a::attr(href)')
+        peps = response.css(
+            'section[id=numerical-index] tbody a::attr(href)'
+        ).getall()
         for link in peps:
-            yield response.follow(link, callback=self.parse_pep)
+            yield response.follow(
+                urljoin(self.start_urls[0], link, '/'),
+                callback=self.parse_pep
+            )
 
     def parse_pep(self, response):
         """Парсит страницы с документами и формирует Items"""
@@ -20,6 +27,6 @@ class PepSpider(scrapy.Spider):
         data = {
             'name': name,
             'number': name.split('–')[0].replace('PEP', '').strip(),
-            'status': response.css('dt:contains("Status") + dd::text').get()
+            'status': response.css('abbr::text').get()
         }
         yield PepParseItem(data)
